b776da832419b3d162c1495b651b7a4e5f5c0c85
lixunguang <29397924@qq.com>
2023-06-26 20:56:14 +0800

    -
    -

--- pages/location/add.vue
+++ pages/location/add.vue
@@ -17,9 +17,22 @@
 	export default {
 		data() {
 			return {
-				title: 'location'
+	
+				id: 0,
+				title: 'map',
+				latitude: 39.909,
+				longitude: 116.39742,
+				covers: [{
+					latitude: 39.909,
+					longitude: 116.39742,
+					iconPath: '../../../static/location.png'
+				}, {
+					latitude: 39.90,
+					longitude: 116.39,
+					iconPath: '../../../static/location.png'
+				}]
 
-		}
+			}
 
 		},
 
