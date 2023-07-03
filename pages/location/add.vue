772beb6f2c9e2faa2dd4bed29fa77f51b7e3e43c
lixunguang <lixunguang@whut.edu.cn>
2023-07-03 09:54:26 +0800

    zhoumo

--- pages/location/add.vue
+++ pages/location/add.vue
@@ -166,6 +166,10 @@ console.log(common.g_i)
 		updated() {
 			console.log('update ----')
 		},
+		
+		
+	 
+		 
 		methods: {
 			getCommit() {
 				console.log('commit')
@@ -175,10 +179,43 @@ console.log(common.g_i)
 			},
 			commitClick() {
 
-				//this.$forceUpdate();
+				 
 				console.log('commitClick -->')
 
 				//this.addLocation();
+				
+				var _this = this;
+				//调用获取城市列表接口
+				qqmapsdk.getCityList({
+				  success: function(res) {//成功后的回调
+				    //console.log(res);
+				    //console.log('省份数据：', res.result[0]); //打印省份数据
+				    //console.log('城市数据：', res.result[1]); //打印城市数据
+				    //console.log('区县数据：', res.result[2]); //打印区县数据
+				  },
+				  fail: function(error) {
+				    console.error(error);
+				  },
+				  complete: function(res) {
+				   // console.log(res);
+				  }
+				});
+				
+				qqmapsdk.getDistrictByCityId({
+				          // 传入对应省份ID获得城市数据，传入城市ID获得区县数据,依次类推
+				          id: 371700, //对应接口getCityList返回数据的Id，如：北京是'110000'
+				          success: function(res) {//成功后的回调
+				            console.log(res);
+				            console.log('对应城市ID下的区县数据(以北京为例)：', res.result[0]);
+				          },
+				          fail: function(error) {
+				            console.error(error);
+				          },
+				          complete: function(res) {
+				            console.log(res);
+				          }
+				        });
+				
 				
 				 // 调用接口
 			/*
